9121f598bf00c092ab790b529cacd6ad3f2c6e43
imagh <akashg.sde@gmail.com>
2024-02-03 15:35:38 +0530

    enhancement: users: applied authguard
    Signed-off-by: imagh <akashg.sde@gmail.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Request, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDTO } from './dto/users.dto';
-// import { User } from './interfaces/users.interface';
 import { User } from "./schemas/users.schema";
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
+
+  @Get('profile')
+  async getProfile(@Request() req) {
+    return this.usersService.findById(req.user.id);
+  }
 
   @Get()
   async findAll(): Promise<User[]> {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,21 @@ import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 import { User, UserSchema } from "./schemas/users.schema";
 import { MongooseModule } from "@nestjs/mongoose";
+import { APP_GUARD } from "@nestjs/core";
+import { AuthGuard } from "src/auth/auth.guard";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }], "core")
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    },
+    UsersService
+  ],
   exports: [UsersService]
 })
 
